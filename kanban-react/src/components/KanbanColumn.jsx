f6f3ca7b4d12ecfd797b7c25d8043714a50c0ac3
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-07 16:05:33 -0300

    feat(components): adicionar KanbanColumn (botão de adicionar, TaskCard e TaskFormModal)

--- kanban-react/src/components/KanbanColumn.jsx
+++ kanban-react/src/components/KanbanColumn.jsx
@@ -6,12 +6,12 @@ export default function KanbanColumn({ title, tasks, status, onAddTask }) {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="bg-white p-4 rounded-2xl shadow-md">
+    <div className="bg-white p-4 rounded-2xl shadow-md w-72 md:w-120 sm:w-full">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-semibold text-gray-700">{title}</h2>
         <button
           onClick={() => setShowModal(true)}
-          className="bg-blue-500 text-white px-3 py-1 rounded-lg hover:bg-blue-600 transition"
+          className="bg-blue-500 text-white cursor-pointer px-3 py-1 rounded-lg hover:bg-blue-600 transition"
         >
           +
         </button>
