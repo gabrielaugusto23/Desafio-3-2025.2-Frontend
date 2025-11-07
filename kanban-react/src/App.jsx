603b969f320271f237e8e4a1457aad2a8cc7a20b
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-07 14:00:48 -0300

    feat(app): aplicar Tailwind em App.jsx e importar App.css

--- kanban-react/src/App.jsx
+++ kanban-react/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <div>
       <h1>Kanban Board</h1>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind is working 🎉
+      </h1>
     </div>
+    
   );
 }
 
