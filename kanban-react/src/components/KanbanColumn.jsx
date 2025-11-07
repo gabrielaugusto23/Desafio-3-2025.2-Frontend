da2894c1a3d795556bb409c3aa91abb3cb085d56
Gabriel Augusto Vilar <me.gabrielvilar@gmail.com>
2025-11-07 16:33:47 -0300

    feat(components): adicionar scrollbar customizado

--- kanban-react/src/components/KanbanColumn.jsx
+++ kanban-react/src/components/KanbanColumn.jsx
@@ -4,7 +4,6 @@ import TaskFormModal from "./TaskForm";
 
 export default function KanbanColumn({ title, tasks, status, onAddTask }) {
   const [showModal, setShowModal] = useState(false);
-
   return (
     <div className="bg-white p-4 rounded-2xl shadow-md w-72 md:w-120 sm:w-full">
       <div className="flex justify-between items-center mb-4">
@@ -16,13 +15,22 @@ export default function KanbanColumn({ title, tasks, status, onAddTask }) {
           +
         </button>
       </div>
-
-      {tasks.length > 0 ? (
-        tasks.map((task) => <TaskCard key={task.id} task={task} />)
-      ) : (
-        <p className="text-gray-400 text-center italic">Sem tarefas</p>
-      )}
-
+      <div className="
+        space-y-3 
+        max-h-[708px] 
+        overflow-y-auto 
+        pr-1 
+        scrollbar-thin 
+        scrollbar-thumb-gray-400 
+        scrollbar-track-transparent 
+        hover:scrollbar-thumb-gray-500
+      ">
+        {tasks.length > 0 ? (
+          tasks.map((task) => <TaskCard key={task.id} task={task} />)
+        ) : (
+          <p className="text-gray-400 text-center italic">Sem tarefas</p>
+        )}
+      </div>
       {showModal && (
         <TaskFormModal
           status={status}
